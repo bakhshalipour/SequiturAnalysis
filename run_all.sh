5fe26d40f38863f34d13c365354bf453fe15b985
bakhshalipour <m.bakhshalipour@gmail.com>
2020-06-29 13:19:29 -0400

    Corrected indent issue in the run script

--- run_all.sh
+++ run_all.sh
@@ -12,12 +12,12 @@ g++ -std=c++11 analyze_stream_length.cpp -O3 -o analyze_stream_length.o
 
 for trace in $(ls Traces/)
 do
-	input_file="Traces/$trace"
+    input_file="Traces/$trace"
     trace_name=${trace%.*}
-	echo "Trace: $trace_name"
-	./$SEQUITUR -m 5000 -p -d < $input_file > "$trace_name-grammar.txt"
+    echo "Trace: $trace_name"
+    ./$SEQUITUR -m 5000 -p -d < $input_file > "$trace_name-grammar.txt"
     echo "+++++++++++++++++++++++++++++++++++++++++++++++++++"
-	./analyze_opportunity.o "$trace_name-grammar.txt" $(wc -l < $input_file)
+    ./analyze_opportunity.o "$trace_name-grammar.txt" $(wc -l < $input_file)
     echo "+++++++++++++++++++++++++++++++++++++++++++++++++++"
     ./analyze_stream_length.o "$trace_name-grammar.txt"
     echo "---------------------------------------------------"
